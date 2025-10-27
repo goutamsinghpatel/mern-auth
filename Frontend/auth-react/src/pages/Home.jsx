9648f21764a6c67ee229b2767e5846775ea55587
banna <goutamsinghpatel2005@gmail.com>
2025-10-27 21:28:01 +0530

    frontend change

--- Frontend/auth-react/src/pages/Home.jsx
+++ Frontend/auth-react/src/pages/Home.jsx
@@ -4,7 +4,7 @@ import Navbar from "../component/Navbar";
 export default function Home(){
     return(
         <>
-     <div className=" flex flex-col items-center justify-center min-h-screen bg-[url('/photos/bg_img.png</>')] bg-cover bg-center">
+     <div className=" flex flex-col items-center justify-center min-h-screen bg-[url('/photos/bg_img.png')] bg-cover bg-center">
           <Navbar/>
           <Header/>
      </div>
