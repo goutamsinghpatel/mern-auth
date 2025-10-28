16a2b85e9d2bd4fafa2a06a0a75f0342c2b1d1a7
banna <goutamsinghpatel2005@gmail.com>
2025-10-28 18:06:14 +0530

    ok

--- Frontend/auth-react/src/pages/Home.jsx
+++ Frontend/auth-react/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import Header from "../component/Header";
 import Navbar from "../component/Navbar";
 
 export default function Home(){
+
     return(
         <>
      <div className=" flex flex-col items-center justify-center min-h-screen bg-[url('/photos/bg_img.png')] bg-cover bg-center">
