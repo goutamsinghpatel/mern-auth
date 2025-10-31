38ff8c4a9adfc02f3ff2d853049cf612851f9b83
banna <goutamsinghpatel2005@gmail.com>
2025-10-31 14:02:37 +0530

    banna

--- Backend/controllers/authcontroller.js
+++ Backend/controllers/authcontroller.js
@@ -83,8 +83,8 @@ if(!isMatch){
 //   here the expirein remove the awt token and afet 7 d it come and login ;
 res.cookie('token',token,
     {httpOnly:true,
-    secure:process.env.NODE_ENV==='production',
-    sameSite:process.env.NODE_ENV==='production'?'none':'strict',
+    secure:true,
+    sameSite:'none',
     maxAge:7*24*60*60*1000,
 
 });
